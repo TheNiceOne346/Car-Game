bd8827fb6bd13e0f9fce8712600120553f65b7c0
TheNiceOne346 <145574490+TheNiceOne346@users.noreply.github.com>
2023-09-28 10:14:26 +0200

    EndMenu

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject FinishMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FinishMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D Other)
+    {
+        if (Other.gameObject.CompareTag("Car"))
+        {
+            Debug.Log("Finish");
+            enableMenu();
+        }
+    }
+    private void enableMenu()
+    {
+        Time.timeScale = 0;
+        FinishMenu.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameoverMenu : MonoBehaviour
+{
+    public void OnPressRetry()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
