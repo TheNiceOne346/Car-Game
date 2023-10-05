df60520f36c042b55cd2e774c37c893dbe0b0bb3
TheNiceOne346 <145574490+TheNiceOne346@users.noreply.github.com>
2023-10-05 12:53:20 +0200

    Main menu fix

--- Assets/Scripts/IfCocaCola.cs
+++ Assets/Scripts/IfCocaCola.cs
@@ -6,6 +6,17 @@ using UnityEngine;
 public class IfCocaCola : MonoBehaviour
 {
     
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Car"))
+        {
+            other.GetComponent<SpriteRenderer>().color = Color.magenta;
+            Debug.Log("Easter Egg Works");
+        }
+        Debug.Log("TEST");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +29,4 @@ public class IfCocaCola : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        
-        Debug.Log("Easter Egg Works");
-    }
 }
